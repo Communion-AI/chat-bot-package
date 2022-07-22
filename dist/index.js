9ba88a37419ed44d07c78216a2ca49bdef2af7ee
James <james@jamess-mbp-2.lan>
2022-07-22 16:34:54 -0400

    .g

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,13 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "Button", {
+Object.defineProperty(exports, "ChatBox", {
   enumerable: true,
   get: function get() {
-    return _Button.default;
+    return _ChatBox.default;
   }
 });
 
-var _Button = _interopRequireDefault(require("./components/Button"));
+var _ChatBox = _interopRequireDefault(require("./components/ChatBox"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
